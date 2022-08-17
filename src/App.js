d3b5946bdd7d8d49fe925c2148e3b3d8dd877ca1
lDrex1 <damzealwini7@gmail.com>
2022-08-17 13:04:21 +0100

    Sign Up component added to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { useState, useRef, useEffect } from "react";
 import "./App.css";
 import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
 import { db } from "./components/firebase-config";
 import { collection, getDocs } from "@firebase/firestore";
 
 function App() {
   return (
     <>
-      <SignUp />
+      {/* <SignUp /> */}
+      <SignIn />
     </>
   );
 }
