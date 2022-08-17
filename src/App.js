9a50e47b5066645657d11f394982ba5d48e04879
lDrex1 <damzealwini7@gmail.com>
2022-08-17 01:46:24 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
 import SignUp from "./components/SignUp";
+import { db } from "./components/firebase-config";
+import { collection, getDocs } from "@firebase/firestore";
 
 function App() {
   return (
