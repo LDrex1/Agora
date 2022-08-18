765c1795f3af8e3598c0178c325bfe7d76dfc3a1
lDrex1 <damzealwini7@gmail.com>
2022-08-18 23:14:01 +0100

    IntroductionPages componet imported and returned

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import { useState, useRef, useEffect } from "react";
 import "./App.css";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
+import SignOut from "./components/SignOut";
+import IntroductionPages from "./components/IntroductionPages";
 import { db } from "./components/firebase-config";
 import { collection, getDocs } from "@firebase/firestore";
 
 function App() {
+  const signOutContainerStyle = { marginTop: "20px", marginLeft: "10px" };
   return (
     <>
-      {/* <SignUp /> */}
-      <SignIn />
+      {/* <SignOut signOutContainerStyle={signOutContainerStyle} />
+      <SignUp />
+      <SignIn /> */}
+      <IntroductionPages />
     </>
   );
 }
