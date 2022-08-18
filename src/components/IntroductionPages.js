77ed746ff69a7d76bf13c74cfdb6400a1177063c
lDrex1 <damzealwini7@gmail.com>
2022-08-18 23:13:19 +0100

    new component

---
+++ src/components/IntroductionPages.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Welcome from "./Welcome";
+
+function IntroductionPages() {
+  return (
+    <>
+      <Welcome />
+    </>
+  );
+}
+
+export default IntroductionPages;
---
+++ src/components/Welcome.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+function Welcome() {
+  return (
+    <Wrapper>
+      <H1 className="text-center">WELCOME TO AGORA</H1>
+    </Wrapper>
+  );
+}
+
+export default Welcome;
+
+//styles
+
+const Wrapper = styled.div``;
+
+const H1 = styled.h1``;
