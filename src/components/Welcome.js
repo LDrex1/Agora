c4b963c5fa126545bc218e9d6928873ddd6bff82
lDrex1 <damzealwini7@gmail.com>
2022-08-20 02:15:57 +0100

    slides content

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,35 @@ import styled from "styled-components";
 function Welcome() {
   return (
     <Wrapper>
-      <H1 className="text-center">WELCOME TO AGORA</H1>
+      <Container className="slide-1">
+        <H1 className="text-center">WELCOME TO AGORA</H1>
+        <ImageDiv></ImageDiv>
+        <Div>
+          <P>
+            We know you are eager to start sharing your ideas but please take
+            time out to read tips on how to use the app.
+          </P>
+        </Div>
+      </Container>
+
+      <Container className="slide 2">
+        <H1 className="text-center">Join a Chat Room</H1>
+        <ImageDiv></ImageDiv>
+        <Div>
+          <P>
+            There are different chat rooms and you are allowed to enter any room
+            of your choise.
+          </P>
+        </Div>
+      </Container>
+
+      <Container className="slide3">
+        <H1 className="text-center">Time to open up!</H1>
+        <ImageDiv></ImageDiv>
+        <Div>
+          <P>Thank you for your patience, you can now proceed to Agora</P>
+        </Div>
+      </Container>
     </Wrapper>
   );
 }
@@ -15,4 +43,14 @@ export default Welcome;
 
 const Wrapper = styled.div``;
 
+const Container = styled.div``;
+
 const H1 = styled.h1``;
+
+const ImageDiv = styled.div`
+  background-image: ;
+`;
+
+const Div = styled.div``;
+
+const P = styled.p``;
